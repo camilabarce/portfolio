3a2a2b0894653fee089abe540600c7bb43aa19d3
Camila Arce <arce.b.camila@gmail.com>
2024-04-19 19:58:35 -0300

    agregar libreria Toastify

--- js/main.js
+++ js/main.js
@@ -4,3 +4,37 @@ suitch.addEventListener("click", e => {
     suitch.classList.toggle("active");
     document.body.classList.toggle("active");
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Verifica si el hash de la URL es "#contacto", lo que indica que se ha redirigido a la sección de contacto
+    if (window.location.hash === '#contacto') {
+        // Muestra el mensaje de Toastify
+        Toastify({
+            text: "¡Se envió el mensaje exitosamente!",
+            duration: 3500,
+            style: {
+                background: "linear-gradient(to right, #eee0cd ,#f2f2f2)",
+                borderRadius: "1rem",
+                color: "#464646"
+            },
+            gravity: "top", 
+            position: "center"
+        }).showToast();
+    }
+});
+
+// function mostrarMensaje () {
+//     Toastify({
+//         text: "¡Se envió el mensaje exitosamente!",
+//         duration: 3500,
+//         style: {
+//             background: "linear-gradient(to right, #eee0cd ,#f2f2f2)",
+//             borderRadius: "1rem",
+//             color: "#464646"
+//         },
+//         gravity: "top", 
+//         position: "center"
+//     }).showToast();
+// }
+
+// mostrarMensaje();
