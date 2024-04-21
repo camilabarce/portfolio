ebd71b2cea1a6f80d94c845ec8f2ec5511768372
Camila Arce <arce.b.camila@gmail.com>
2024-04-21 17:26:41 -0300

    guardar darkmode en local storage

--- js/main.js
+++ js/main.js
@@ -1,40 +1,41 @@
 const suitch = document.querySelector(".switch");
 
-suitch.addEventListener("click", e => {
-    suitch.classList.toggle("active");
-    document.body.classList.toggle("active");
+document.addEventListener("DOMContentLoaded", e => {
+    cargarDarkMode()
+    suitch.addEventListener("click", toggleDarkMode)
 })
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Verifica si el hash de la URL es "#contacto", lo que indica que se ha redirigido a la sección de contacto
-    if (window.location.hash === '#contacto') {
-        // Muestra el mensaje de Toastify
-        Toastify({
-            text: "¡Se envió el mensaje exitosamente!",
-            duration: 3500,
-            style: {
-                background: "linear-gradient(to right, #eee0cd ,#f2f2f2)",
-                borderRadius: "1rem",
-                color: "#464646"
-            },
-            gravity: "top", 
-            position: "center"
-        }).showToast();
+function toggleDarkMode() {
+    suitch.classList.toggle("active");
+    document.body.classList.toggle("active");
+    guardarDarkMode(suitch.classList.contains("active"))
+}
+
+function guardarDarkMode(estado) {
+    localStorage.setItem("darkMode", estado)
+}
+
+function cargarDarkMode() {
+    const darkModeGuardado = localStorage.getItem("darkMode") === "true";
+    if (darkModeGuardado) {
+        suitch.classList.add("active");
+        document.body.classList.add("active");
     }
-});
-
-// function mostrarMensaje () {
-//     Toastify({
-//         text: "¡Se envió el mensaje exitosamente!",
-//         duration: 3500,
-//         style: {
-//             background: "linear-gradient(to right, #eee0cd ,#f2f2f2)",
-//             borderRadius: "1rem",
-//             color: "#464646"
-//         },
-//         gravity: "top", 
-//         position: "center"
-//     }).showToast();
-// }
-
-// mostrarMensaje();
+}
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Verifica si el hash de la URL es "#contacto", lo que indica que se ha redirigido a la sección de contacto
+//     if (window.location.hash === '#contacto') {
+//         // Muestra el mensaje de Toastify
+//         Toastify({
+//             text: "¡Se envió el mensaje exitosamente!",
+//             duration: 3500,
+//             style: {
+//                 background: "linear-gradient(to right, #eee0cd ,#f2f2f2)",
+//                 borderRadius: "1rem",
+//                 color: "#464646"
+//             },
+//             gravity: "top",
+//             position: "center"
+//         }).showToast();
+//     }
+// });
